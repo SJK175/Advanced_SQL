395828735b1027a1f060395b6e3c81d841720c46
Subhajit Kundu <mrsubhajitkundu@gmail.com>
2022-09-15 18:52:23 +0530

    Create P1_PointsTableICCTournament.sql

---
+++ P1_PointsTableICCTournament.sql
@@ -0,0 +1,28 @@
+/*Create the table*/
+create table icc_world_cup
+(
+Team_1 Varchar(20),
+Team_2 Varchar(20),
+Winner Varchar(20)
+);
+INSERT INTO icc_world_cup values('India','SL','India');
+INSERT INTO icc_world_cup values('SL','Aus','Aus');
+INSERT INTO icc_world_cup values('SA','Eng','Eng');
+INSERT INTO icc_world_cup values('Eng','NZ','NZ');
+INSERT INTO icc_world_cup values('Aus','India','India');
+
+/*output*/
+select team_name
+, sum(played) as total_played
+, sum(win_count) as total_win
+from
+(select Team_1 as team_name
+,case when Team_1 = Winner then 1 else 0 end as win_count
+,1 as played  
+from icc_world_cup
+union all
+select Team_2 as team_name
+,case when Team_2 = Winner then 1 else 0 end as win_count
+,1 as played  
+from icc_world_cup) as t1
+group by team_name
