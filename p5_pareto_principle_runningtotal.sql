2fa3e8f120ba64a1e4c6bfdba1c6c79bbfeff5a8
Subhajit Kundu <mrsubhajitkundu@gmail.com>
2022-09-29 13:11:31 +0530

    Update p5_pareto_principle_runningtotal.sql

--- p5_pareto_principle_runningtotal.sql
+++ p5_pareto_principle_runningtotal.sql
@@ -1,4 +1,4 @@
---creating the data
+--generating the data
 create table product_sale
 (product_id varchar(20),
 sale integer  );
@@ -14,3 +14,24 @@ insert into product_sale values ("P9", 32);
 insert into product_sale values ("P10", 69);
 insert into product_sale values ("P11", 67);
 insert into product_sale values ("P12", 99);
+
+
+/*selecting top products that cover 80% of sales*/
+with cte1 as 
+(select product_id,sale
+--calculating running_total and total_sale within query
+,sum(sale) over(order by sale desc) as running_tot 
+,sum(sale) over() as total_sale   
+from
+(select product_id
+,sale
+from product_sale
+order by sale desc
+) as t0
+)
+
+select product_id, sale, sale_pct from 
+(select product_id, sale, running_tot, total_sale,
+round((running_tot*1.00/total_sale),4) as sale_pct
+from cte1)
+where sale_pct <= 0.8
