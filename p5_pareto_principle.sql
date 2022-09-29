0543fca11541117c463194ca9403feb0bbaf669e
Subhajit Kundu <mrsubhajitkundu@gmail.com>
2022-09-29 12:22:51 +0530

    Create p5_pareto_principle.sql

---
+++ p5_pareto_principle.sql
@@ -0,0 +1,16 @@
+--creating the data
+create table product_sale
+(product_id varchar(20),
+sale integer  );
+insert into product_sale values ("P1", 209);
+insert into product_sale values ("P2", 332);
+insert into product_sale values ("P3", 34);
+insert into product_sale values ("P4", 673);
+insert into product_sale values ("P5", 102);
+insert into product_sale values ("P6", 187);
+insert into product_sale values ("P7", 509);
+insert into product_sale values ("P8", 442);
+insert into product_sale values ("P9", 32);
+insert into product_sale values ("P10", 69);
+insert into product_sale values ("P11", 67);
+insert into product_sale values ("P12", 99);
